652c8df3b016e36a69a9884a8ec566e898ceb6c5
aniak120 <167074333+aniak120@users.noreply.github.com>
2024-04-25 20:11:47 +0200

    Add files via upload

--- optimal.py
+++ optimal.py
@@ -12,7 +12,7 @@ import datetime
 timezone = "Europe/Copenhagen"
 start_date = "2024-01-01"
 end_date = "2024-12-31"
-delta_time = "H"  # "Min", "H", 
+delta_time = "h"  # "Min", "H", 
 
 # Definition of Location object. Coordinates and elevation of Amager, Copenhagen (Denmark)
 site = Location(
@@ -25,13 +25,13 @@ times = pd.date_range(
     start_date + " 00:00", end_date + " 00:00", inclusive="left", freq=delta_time, tz=timezone
 )
 #closed="left", - missing from date range
-sunpos = site.get_solarposition(times)
+# sunpos = site.get_solarposition(times)
 
-theta_sun = np.array(sunpos.zenith)
-phi_sun = np.array(sunpos.azimuth)
-theta_panel = 0
-phi_panel = 180
-#print(sunpos.zenith)
+# theta_sun = np.array(sunpos.zenith)
+# phi_sun = np.array(sunpos.azimuth)
+# theta_panel = 0
+# phi_panel = 180
+# #print(sunpos.zenith)
 
 def solar_panel_projection(theta_sun, phi_sun, theta_panel, phi_panel):
     # convert angles from degrees to radians
@@ -59,42 +59,75 @@ def solar_flux(theta_sun, phi_sun, theta_panel, phi_panel):
             flux_array = np.append(flux_array, 0)
     return flux_array
 
-np.set_printoptions(threshold=np.inf) #printing the whole array
-#print(solar_flux(theta_sun, phi_sun, theta_panel, phi_panel))
+# np.set_printoptions(threshold=np.inf) #printing the whole array
+# #print(solar_flux(theta_sun, phi_sun, theta_panel, phi_panel))
 
-time = np.arange(0, 365*24, 1) #time in minutes
+# time = np.arange(0, 365*24, 1) #time in minutes
 
-# Plot for solar flux over time
-# fig, ax2 = plt.subplots(figsize=(10, 5))
-# ax2.plot(time, solar_flux(theta_sun, phi_sun, theta_panel, phi_panel))
-# ax2.set_ylabel("Solar Flux")
-# ax2.set_xlabel("Time (hours)")
-# ax2.set_title("Solar Flux over Time")
-#plt.show()
+# # Plot for solar flux over time
+# # fig, ax2 = plt.subplots(figsize=(10, 5))
+# # ax2.plot(time, solar_flux(theta_sun, phi_sun, theta_panel, phi_panel))
+# # ax2.set_ylabel("Solar Flux")
+# # ax2.set_xlabel("Time (hours)")
+# # ax2.set_title("Solar Flux over Time")
+# #plt.show()
 
 
-#print(integral_value)
+# #print(integral_value)
 
-integral_value = integrate.simps(solar_flux(theta_sun, phi_sun, theta_panel, phi_panel),time, dx=60)
+# integral_value = integrate.simps(solar_flux(theta_sun, phi_sun, theta_panel, phi_panel),time, dx=3600)
 
-#print(integral_value)
-int_values=np.array([])
-for i in range(0, 90):
-    theta_panel = i
-    integral_value = integrate.simps(solar_flux(theta_sun, phi_sun, theta_panel, phi_panel),time, dx=60)
-    int_values = np.append(int_values, integral_value)
-#print(int_values)
+# #print(integral_value)
+# int_values=np.array([])
+# for i in range(0, 90):
+#     theta_panel = i
+#     integral_value = integrate.simps(solar_flux(theta_sun, phi_sun, theta_panel, phi_panel),time, dx=3600)
+#     int_values = np.append(int_values, integral_value)
+# #print(int_values)
 
-angles = np.arange(0, 90, 1) #angles
-# Plot for integrals over degrees
-fig, ax3 = plt.subplots(figsize=(10, 5))
-ax3.plot(angles, int_values)
-ax3.set_ylabel("Integrals")
-ax3.set_xlabel("Theta_p angle (degrees)")
-ax3.set_title("Integrals over theta_p angle")
+# angles = np.arange(0, 90, 1) #angles
+# # Plot for integrals over degrees
+# fig, ax3 = plt.subplots(figsize=(10, 5))
+# ax3.plot(angles, int_values)
+# ax3.set_ylabel("Integrals (W/m^2)")
+# ax3.set_xlabel("Theta_p angle (degrees)")
+# ax3.set_title("Integrals over theta_p angle")
 
-#plt.show()
+# plt.show()
 
-max_prod=np.max(int_values)
-optimal_angle=np.argmax(int_values)
-print("The optimal angle is: ", optimal_angle, "degrees")
+# max_prod=np.max(int_values)
+# optimal_angle=np.argmax(int_values)
+# print("The optimal angle is: ", optimal_angle, "degrees")
+# print(max_prod)
+
+number_of_panels=20
+panels_area=2.16
+total_area=number_of_panels*panels_area
+
+theta_panel = 55
+phi_panel = 180
+
+times = pd.date_range(
+    start_date, end_date)
+#print(times.date.tolist())
+energy_production = []
+for day in times.date.tolist():
+    delta_time = "Min" 
+    time = pd.date_range(
+    start_date + " 00:00:00", end_date + " 23:59:00", inclusive="left", freq=delta_time)
+    sunpos = site.get_solarposition(time)
+    chosen_date = str(day)#.strftime("%Y-%m-%d")
+    #print(chosen_date)
+    daily_energy = integrate.simps(solar_flux(sunpos.loc[chosen_date].zenith, sunpos.loc[chosen_date].azimuth, theta_panel, phi_panel), np.arange(0, 24*60, 1), dx=60)
+    print(daily_energy)
+#     energy_production.append(daily_energy)
+# print(energy_production)
+# fig, ax = plt.subplots(figsize=(10, 5))
+# ax.plot(times.date.tolist(), energy_production)
+# ax.set_ylabel("Energy Production (W/m^2)")
+# ax.set_xlabel("Date")
+# ax.set_title("Energy Production per Day")
+# ax.xaxis.set_major_locator(mdates.MonthLocator())
+# ax.xaxis.set_major_formatter(mdates.DateFormatter("%b %Y"))
+# plt.xticks(rotation=45)
+# plt.show()
