923a331c54e154230aee9564e805aa97abb3fb1b
aniak120 <167074333+aniak120@users.noreply.github.com>
2024-04-26 18:16:52 +0200

    Add files via upload

--- power.py
+++ power.py
@@ -27,7 +27,7 @@ times = pd.date_range(
 #closed="left", - missing from date range
 sunpos = site.get_solarposition(times)
 
-chosen_date = "2024-04-01"
+chosen_date = "2024-04-20"
 #print(sunpos.loc[chosen_date].zenith)
 
 
@@ -45,23 +45,17 @@ def solar_panel_projection(theta_sun, phi_sun, theta_panel, phi_panel):
     theta_panel = np.deg2rad(theta_panel)
     phi_panel = np.deg2rad(phi_panel)
 
-    #radius=pvlib.solarposition.nrel_earthsun_distance(times) * 149597870700 #distance from sun to earth in meters
-    #r=radius.loc[chosen_date]
     # calculate the unit vectors
     u_s = np.array([np.sin(theta_sun) * np.cos(phi_sun), np.sin(theta_sun) * np.sin(phi_sun), np.cos(theta_sun)])
     u_p = np.array([np.sin(theta_panel) * np.cos(phi_panel), np.sin(theta_panel) * np.sin(phi_panel), np.cos(theta_panel)])
 
     # calculate the dot product
-    #dot_product = np.dot(u_s, u_p)
-
     dot_products=u_p[0]*np.array(u_s[0])+u_p[1]*np.array(u_s[1])+u_p[2]*np.array(u_s[2])
-    #dot_sum = np.sum(dot_multiplication, axis=0)
-    # return the dot product if it's positive, or zero otherwise
-    #return max(0, dot_products)
     return dot_products
 
 #print(solar_panel_projection(theta_sun, phi_sun, theta_panel, phi_panel))
 
+#CALCULATING POWER BY SOLAR FLUX
 def solar_flux(theta_sun, phi_sun, theta_panel, phi_panel):
     dot_products=solar_panel_projection(theta_sun, phi_sun, theta_panel, phi_panel)
     #flux_array=np.empty_like(dot_products)
@@ -79,40 +73,41 @@ np.set_printoptions(threshold=np.inf) #printing the whole array
 
 time = np.arange(0, 24*60, 1) #time in minutes
 
-# # Plots for solar zenith and solar azimuth angles
-# fig, (ax1) = plt.subplots(1, 1, figsize=(30, 10))
-# fig.suptitle("Solar Energy production Estimation in " + site.name + " " + chosen_date)
+# Plots for solar zenith and solar azimuth angles
+fig, (ax1) = plt.subplots(1, 1, figsize=(30, 10))
+fig.suptitle("Solar Energy production Estimation in " + site.name + " " + chosen_date)
 
-# # plot for solar zenith angle
-# ax1.plot(solar_flux(theta_sun, phi_sun, theta_panel, phi_panel))
-# ax1.set_ylabel("Energy")
-# ax1.set_xlabel("Time UTC+2 (hour)")
-# # Get the current x-axis tick positions and labels
-# ticks = ax1.get_xticks()
-# tick_labels = ax1.get_xticklabels()
-# # Convert the tick positions to datetime objects
-# tick_datetimes = [mdates.num2date(tick) for tick in ticks]
-# # Add 2 hours to each datetime object
-# new_tick_datetimes = [tick_datetime + datetime.timedelta(hours=2) for tick_datetime in tick_datetimes]
-# # Convert the updated datetime objects back to numerical values
-# new_ticks = mdates.date2num(new_tick_datetimes)
-# # Set the new tick positions and labels
-# ax1.set_xticks(ticks)
-# ax1.set_xticklabels([tick_label.strftime('%H') for tick_label in new_tick_datetimes])
+# plot for solar zenith angle
+ax1.plot(solar_flux(theta_sun, phi_sun, theta_panel, phi_panel))
+ax1.set_ylabel("Energy")
+ax1.set_xlabel("Time UTC+2 (hour)")
+# Get the current x-axis tick positions and labels
+ticks = ax1.get_xticks()
+tick_labels = ax1.get_xticklabels()
+# Convert the tick positions to datetime objects
+tick_datetimes = [mdates.num2date(tick) for tick in ticks]
+# Add 2 hours to each datetime object
+new_tick_datetimes = [tick_datetime + datetime.timedelta(hours=2) for tick_datetime in tick_datetimes]
+# Convert the updated datetime objects back to numerical values
+new_ticks = mdates.date2num(new_tick_datetimes)
+# Set the new tick positions and labels
+ax1.set_xticks(ticks)
+ax1.set_xticklabels([tick_label.strftime('%H') for tick_label in new_tick_datetimes])
 
-# Plot for solar flux over time
+# PLOT FOR SOLAR FLUX OVER TIME
 fig, ax2 = plt.subplots(figsize=(10, 5))
 ax2.plot(time, solar_flux(theta_sun, phi_sun, theta_panel, phi_panel))
-ax2.set_ylabel("Solar Flux")
-ax2.set_xlabel("Time (minutes)")
-ax2.set_title("Solar Flux over Time")
+ax2.set_ylabel("Solar Flux (W/m^2)")
+ax2.set_xlabel("UTC Time (minutes)")
+ax2.set_title("Solar Flux over Time on 20th of April 2024 DTU 303")
 #plt.show()
 
+#CALCULATING ENERGY PRODUCTION
 integral_value = integrate.simps(solar_flux(theta_sun, phi_sun, theta_panel, phi_panel),time, dx=60)
 
 #print(integral_value)
 int_values=np.array([])
-dot_products=np.array([])
+#dot_products=np.array([])
 for i in range(0, 90):
     theta_panel = i
     # dot_product=solar_panel_projection(theta_sun, phi_sun, theta_panel, phi_panel)
@@ -122,15 +117,15 @@ for i in range(0, 90):
     int_values = np.append(int_values, integral_value)
 
 
-
+#PLOT ENERGY PRODUCTION OVER ANGLES
 #print(int_values)
 #print(dot_products)
 angles = np.arange(0, 90, 1) #angles
 # Plot for integrals over degrees
 fig, ax3 = plt.subplots(figsize=(10, 5))
 ax3.plot(angles, int_values)
-ax3.set_ylabel("Integrals")
+ax3.set_ylabel("Energy production (Wh/m^2)")
 ax3.set_xlabel("Theta_p angle (degrees)")
-ax3.set_title("Integrals over theta_p angle")
+ax3.set_title("Daily energy production over theta_p angle")
 
 plt.show()
